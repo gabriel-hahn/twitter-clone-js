1c31d6d9dade419af0d2eda303452817d1193145
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-11-07 21:07:19 -0200

    Initial Login component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 
+import twitterLogo from '../twitter.svg';
+import './Login.css';
+
 export default class Login extends Component {
+
+    state = {
+        username: '',
+    };
+
+    handleInputChange = e => {
+        this.setState({ username: e.target.value });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const { username } = this.state;
+        if (!username.length) return;
+
+        localStorage.setItem('@twitter:username', username);
+
+        this.props.history.push('/timeline');
+    }
+
     render() {
-        return <div>Login</div>;
+        return (
+            <div className="login-wrapper">
+                <img src={twitterLogo} alt="Twitter" />
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="User name" value={this.state.username} onChange={this.handleInputChange} />
+                    <button type="submit">Log in</button>
+                </form>
+            </div>
+        );
     }
 }
