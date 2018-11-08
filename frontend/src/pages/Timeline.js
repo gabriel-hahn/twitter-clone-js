4be0b6c6714bac79a0c70e3001d6f3b67f73420e
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-11-07 22:01:06 -0200

    Get all tweets from api and list them

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -4,9 +4,17 @@ import api from '../services/api';
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
 
+import Tweet from '../components/Tweet';
+
 export default class Timeline extends Component {
     state = {
+        tweets: [],
         newTweet: '',
+    };
+
+    async componentDidMount() {
+        const response = await api.get('tweets');
+        this.setState({ tweets: response.data });
     };
 
     handleInputChange = e => {
@@ -31,6 +39,11 @@ export default class Timeline extends Component {
                 <form>
                     <textarea placeholder="What is going on?" value={this.state.newTweet} onChange={this.handleInputChange} onKeyDown={this.handleNewTweet} />
                 </form>
+                <ul className="tweet-list">
+                    {this.state.tweets.map(tweet =>
+                        <Tweet key={tweet._id} tweet={tweet} />
+                    )}
+                </ul>
             </div>
         );
     };
