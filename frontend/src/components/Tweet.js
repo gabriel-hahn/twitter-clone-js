f0baf177825719709e9294b9fcb0152d1643ea7d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-11-07 22:22:56 -0200

    Real-time socket.io implemented

--- frontend/src/components/Tweet.js
+++ frontend/src/components/Tweet.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 import like from '../like.svg';
 import './Tweet.css';
+import api from '../services/api';
 
 export default class Tweet extends Component {
+    handleLike = async () => {
+        const { _id } = this.props.tweet;
+        await api.post(`likes/${_id}`);
+    };
+
     render() {
         const { tweet } = this.props;
 
@@ -11,7 +17,7 @@ export default class Tweet extends Component {
             <li className="tweet">
                 <strong>{tweet.author}</strong>
                 <p>{tweet.content}</p>
-                <button type="button">
+                <button type="button" onClick={this.handleLike}>
                     <img src={like} alt="Like" />
                     {tweet.likes}
                 </button>
--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import socket from 'socket.io-client';
 
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
@@ -13,8 +14,25 @@ export default class Timeline extends Component {
     };
 
     async componentDidMount() {
+        this.subscribeToEvents();
+
         const response = await api.get('tweets');
         this.setState({ tweets: response.data });
+    };
+
+    //Real-time events
+    subscribeToEvents = () => {
+        const io = socket('http://localhost:3000');
+
+        io.on('tweet', data => {
+            this.setState({ tweets: [data, ...this.state.tweets] });
+        });
+
+        io.on('like', data => {
+            this.setState({
+                tweets: this.state.tweets.map(tweet => tweet._id === data._id ? data : tweet)
+            });
+        });
     };
 
     handleInputChange = e => {
