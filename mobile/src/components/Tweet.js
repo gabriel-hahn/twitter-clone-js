ec143c199f68868034e2d2decb61bd860efd148d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-11-11 23:22:32 -0200

    Like method and Tweet component

--- mobile/src/components/Tweet.js
+++ mobile/src/components/Tweet.js
@@ -1,4 +1,33 @@
-/*const styles = StyleSheet.create({
+import React, { Component } from 'react';
+import api from '../services/api';
+
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default class Tweet extends Component {
+  handleLike = async () => {
+    const { _id } = this.props.tweet;
+    await api.post(`likes/${_id}`);
+  };
+
+  render() {
+    const { tweet } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text styles={styles.author}>{tweet.author}</Text>
+        <Text styles={styles.content}>{tweet.content}</Text>
+
+        <TouchableOpacity onPress={this.handleLike} style={styles.likeButton}>
+          <Icon name="ios-heart-empty" size={20} color="#999" />
+          <Text style={styles.likeText}>{tweet.likes}</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
   container: {
     padding: 20,
     borderBottomWidth: 1,
@@ -27,4 +56,4 @@
     color: "#999",
     marginLeft: 5
   }
-});*/
+});
--- mobile/src/pages/Timeline.js
+++ mobile/src/pages/Timeline.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+import Tweet from '../components/Tweet';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Timeline extends Component {
+    static navigationOptions = {
+        title: 'Início',
+        headerRight: (
+            <TouchableOpacity onPress={() => { }}>
+                <Icon style={{ marginRight: 20 }} name="add-circle-outline" size={24} color="#4BB0EE"></Icon>
+            </TouchableOpacity>
+        )
+    };
+
+    state = {
+        tweets: []
+    };
+
+    async componentDidMount() {
+        const response = await api.get('tweets');
+        this.setState({ tweets: response.data });
+    }
+
     render() {
-        return <Text>Timeline</Text>;
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={this.state.tweets}
+                    keyExtractor={tweet => tweet._id}
+                    renderItem={({ item }) => <Tweet tweet={item} />} 
+                />
+            </View>
+        );
     }
 }
 
